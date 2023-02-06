ba68727fd4b8ba7366db8e14ec260394b02d9cf4
Ivan Garcia <ivanvlora@gmail.com>
2023-02-06 22:12:59 +0900

    [feature] add posts

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -6,10 +6,12 @@ module.exports = createCoreController("api::post.post", ({ strapi }) => ({
     // thanks to the custom route we have now a slug variable
     // instead of the default id
     const { slug } = ctx.params;
+
     const entity = await strapi.db.query("api::post.post").findOne({
       where: { slug },
-      populate: ["cover"],
+      populate: ["cover", "technologies"],
     });
+
     const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
 
     return this.transformResponse(sanitizedEntity);
